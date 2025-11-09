120afe5fc48a5e3f9f384813d9feab3dc0943b52
Narala.Aditya ram <150006848+Adityaram544@users.noreply.github.com>
2025-11-09 17:53:42 +0530

    Update 2169-count-operations-to-obtain-zero.java

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -2,11 +2,12 @@ class Solution {
     public int countOperations(int num1, int num2) {
         int c=0;
         while(num1!=0 && num2!=0){
-            c+=num1/num2;
-            num1=num1 % num2;
-            int temp=num1;
-            num1=num2;
-            num2=temp;
+            c++;
+            if(num1>=num2){
+                num1-=num2;
+            }else{
+                num2-=num1;
+            }
         }
         return c;
     }
