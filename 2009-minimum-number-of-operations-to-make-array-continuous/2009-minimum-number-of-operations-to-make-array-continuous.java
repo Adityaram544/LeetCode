8f622507e6c14a1208f9169ab46f00c298d2deed
Narala.Aditya ram <150006848+Adityaram544@users.noreply.github.com>
2026-07-14 12:11:35 +0530

    Time: 47 ms (76.1%), Space: 93.7 MB (75.3%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minOperations(int[] nums) {
         int n=nums.length;
-        int minOps=n;
+        int minOps=0;
         Arrays.sort(nums);
         ArrayList<Integer> lst=new ArrayList<>();
         for(int x:nums){
@@ -9,13 +9,13 @@ class Solution {
                 lst.add(x);
             }
         }
-        int r=0;
-        for(int l=0;l<lst.size();l++){
-            while(r<lst.size() && lst.get(r)<lst.get(l)+n){
-                r++;
+        int l=0;
+        for(int r=0;r<lst.size();r++){
+            if(lst.get(r)-lst.get(l)>=n){
+                l++;
             }
-            minOps=Math.min(minOps,n-(r-l));
+            minOps=Math.max(minOps,r-l+1);
         }
-        return minOps;
+        return n-minOps;
     }
 }
